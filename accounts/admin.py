91eb992f1c0795a73143382288dd934a10e2f425
DHIRAJ843 <thakordhiraj970@gmail.com>
2026-07-11 23:38:28 +0530

    useless

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,4 @@
 from django.contrib import admin
-<<<<<<< Updated upstream
-print("jeje meje")
-=======
-print("siddhraj")
->>>>>>> Stashed changes
-# Register your models here.
- bhai tu peli ne reel na mokalto 
 
+
+admin.site.register(CustomUser)
