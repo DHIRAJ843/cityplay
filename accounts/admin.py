f0763bb01237bab4f18f22b38fb49eb7a3ace5d8
DHIRAJ843 <thakordhiraj970@gmail.com>
2026-07-11 23:48:47 +0530

    Update admin.py

--- accounts/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
-
-# Register your custom user model with Django's built-in UserAdmin interface
-admin.site.register(CustomUser, UserAdmin)
