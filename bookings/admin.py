88366a9ac08031e6c0d3f4cb873324bbd029de91
DHIRAJ843 <thakordhiraj970@gmail.com>
2026-07-15 22:12:40 +0530

    today work done

--- bookings/admin.py
+++ bookings/admin.py
@@ -2,5 +2,3 @@ from django.contrib import admin
 from .models import Booking
 admin.site.register(Booking)
 
-#TODO TELL THEN TO RUN THE MIGRATION
-#TODO TELLL THEM TO INSTALL THE pip install "psycopg[pool]"
