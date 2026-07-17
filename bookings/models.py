6f095262e091704b6ec5b46173d359e88946138a
DHIRAJ843 <thakordhiraj970@gmail.com>
2026-07-17 21:54:06 +0530

    database solve

--- bookings/models.py
+++ bookings/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from events.models import Event
 
 class Booking(models.Model):
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='bookings')
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     num_slots = models.PositiveIntegerField(default=1)
     amount_paid = models.DecimalField(max_digits=8, decimal_places=2)
