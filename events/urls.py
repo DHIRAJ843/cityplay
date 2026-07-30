00763ff6f24de187f0fc3637f335796e1b914d64
DHIRAJ843 <thakordhiraj970@gmail.com>
2026-07-30 22:47:45 +0530

    spacing issue solved

--- events/urls.py
+++ events/urls.py
@@ -7,12 +7,3 @@ urlpatterns = [
     path('<int:pk>/', views.event_detail, name='event_detail'),
     path('activity/<slug:slug>/', views.activity_detail, name='activity_detail'),
 ]
-from django.urls import path
-from . import views
-
-app_name = 'events'
-
-urlpatterns = [
-    path('<int:pk>/', views.event_detail, name='event_detail'),
-    path('activity/<slug:slug>/', views.activity_detail, name='activity_detail'),
-]
